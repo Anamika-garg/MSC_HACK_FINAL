1a10f7b7901f8f705f4f36a773d5b10d2f84d7a3
Anamika Garg <anamikagarg2906@gmail.com>
2025-02-10 21:12:23 +0530

    cors

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const userRoutes = require('./router/userRoutes');
-const jobsRoutes = require('./router/jobsRoutes');
-const postRoutes = require('./router/postRoutes');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoutes = require("./router/userRoutes");
+const jobsRoutes = require("./router/jobsRoutes");
+const postRoutes = require("./router/postRoutes");
 const app = express();
 
-app.use(express.json())
-app.use(cors());
-app.use('/api/user', userRoutes)
-app.use('/api/job', jobsRoutes)
-app.use('/api/post', postRoutes)
+app.use(express.json());
 
-app.get('/' , (req,res)=>{
-    res.send("Hello,world")
-})
+app.use(
+  cors({
+    origin: [process.env.LOCALHOST_URL, process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("Database connected Successful!")
-})
+app.use("/api/user", userRoutes);
+app.use("/api/job", jobsRoutes);
+app.use("/api/post", postRoutes);
 
-app.listen(process.env.PORT || 3000 , ()=>{
-    console.log("Port listening on port 3000")
-})
+app.get("/", (req, res) => {
+  res.send("Hello,world");
+});
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("Database connected Successful!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Port listening on port 3000");
+});
