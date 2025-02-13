d5153bd86b800eca6a7c570abe2fe142b2c159eb
Anamika Garg <anamikagarg2906@gmail.com>
2025-02-13 23:19:26 +0530

    jobs

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
 app.use("/api/user", userRoutes);
 app.use("/api/job", jobsRoutes);
